5e6c3a1be48c56d22e7b0460479e4e1b7b1088d6
Mickael Pham <mickael.pham@gmail.com>
2016-12-20 09:35:32 -0800

    Add expense form

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -9,18 +9,12 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('groups', function() {
     this.route('new');
-    // this.route('show', { path: '/:group_id' });
-    // this.route('edit', { path: '/:group_id/edit' });
-    // this.route('addExpense', { path: '/:group_id/add-expense' });
   });
 
   this.route('group', { path: '/groups/:group_id' }, function() {
     this.route('edit');
+    this.route('add-expense');
   });
-
-  // this.route('group', { path: '/groups/:group_id' }, function() {
-  //   this.route('edit');
-  // });
 });
 
 export default Router;
