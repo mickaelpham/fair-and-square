416119cf33f814fb3fdb882fca476686baf9446e
Mickael Pham <mickael.pham@gmail.com>
2016-12-20 08:39:28 -0800

    Refactor routes to use nested resources

--- frontend/app/routes/group/edit.js
+++ frontend/app/routes/group/edit.js
@@ -2,29 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model(params) {
-    return this.store.findRecord('group', params.group_id);
-  },
-
   actions: {
 
     save(properties) {
       let group = this.controller.get('model');
       group.setProperties(properties);
       group.save().then(() => {
-        this.transitionTo('groups.index');
+        this.transitionTo('group', group);
       });
     },
 
     cancel() {
-      this.transitionTo('groups.index');
+      this.transitionTo('group', this.controller.get('model'));
     },
 
     delete() {
       let group = this.controller.get('model');
       group.deleteRecord();
       group.save().then(() => {
-        this.transitionTo('groups.index');
+        this.transitionTo('groups');
       });
     }
 
